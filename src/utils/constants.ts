55f43aae39ad5484362ecbe16c38deb0eb8de12b
Bruno Menegidio <brunomenegidio@hotmail.com>
2025-06-24 14:41:48 -0300

    feat: diretórios padrão para salvar arquivos de backup e arquivos otimizados

---
+++ src/utils/constants.ts
@@ -0,0 +1,5 @@
+import { join } from 'path';
+
+export const ORIGINAL_FILES_PATH = join(process.cwd(), 'files', 'original');
+
+export const OPTIMIZED_FILES_PATH = join(process.cwd(), 'files', 'optimized');
